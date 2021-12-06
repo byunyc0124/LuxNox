6feda48d80af5eacc771b84f92b85d19b3ae23ed
byunyc0124 <baeyonyk124@gmail.com>
2021-12-06 11:08:10 +0900

    main pro

--- app/src/main/java/bycpkn/luxnox/MainActivity.java
+++ app/src/main/java/bycpkn/luxnox/MainActivity.java
@@ -2,13 +2,36 @@ package bycpkn.luxnox;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button startBtn, continueBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startBtn = findViewById(R.id.startButton);
+        continueBtn = findViewById(R.id.continueButton);
+
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, PrologueActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        continueBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
