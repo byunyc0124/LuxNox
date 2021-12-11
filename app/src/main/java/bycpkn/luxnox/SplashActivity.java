ad88f73f81c9bac53fc0323d41bb37f2b2a673ea
byunyc0124 <baeyonyk124@gmail.com>
2021-12-11 21:50:29 +0900

    splash

---
+++ app/src/main/java/bycpkn/luxnox/SplashActivity.java
@@ -0,0 +1,34 @@
+package bycpkn.luxnox;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+public class SplashActivity extends AppCompatActivity {
+
+    ImageView iv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        iv = findViewById(R.id.splash_IV);
+        Glide.with(this).load(R.drawable.splash).into(iv);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2500);
+    }
+}
