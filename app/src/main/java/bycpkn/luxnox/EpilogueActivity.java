ee97c683f6ed712175e6d14dc8e44d683f2c4e89
byunyc0124 <baeyonyk124@gmail.com>
2021-12-12 00:58:40 +0900

    epilogue gif

--- app/src/main/java/bycpkn/luxnox/EpilogueActivity.java
+++ app/src/main/java/bycpkn/luxnox/EpilogueActivity.java
@@ -4,17 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+
 public class EpilogueActivity extends AppCompatActivity {
 
     ImageView backgroundEpilIV;
     ImageView conversationEpilIV;
     TextView conversationEpilTV;
+    TextView conversationIdTV;
+    ImageView eyesIV;
 
     Animation fadeInAnim, fadeOutAnim;
 
@@ -30,13 +35,17 @@ public class EpilogueActivity extends AppCompatActivity {
         startActivity(intent);
 
         backgroundEpilIV = findViewById(R.id.epil_imageView1);    // 변경될 백그라운드 이미지
-        conversationEpilIV = findViewById(R.id.epil_imageView2); // 터치할 수 있는 대화창 이미지
-        conversationEpilTV = findViewById(R.id.epil_textView2);  // 변경될 대화창 텍스트
+        conversationEpilIV = findViewById(R.id.epil_imageView2);  // 터치할 수 있는 대화창 이미지
+        conversationEpilTV = findViewById(R.id.epil_textView2);   // 변경될 대화창 텍스트
+        conversationIdTV = findViewById(R.id.epil_textView1);    // 교수님 텍스트
+        eyesIV = findViewById(R.id.epil_imageView3);              // 깜빡이는 눈
 
         fadeInAnim = AnimationUtils.loadAnimation(this, R.anim.fadein);
         fadeOutAnim = AnimationUtils.loadAnimation(this, R.anim.fadeout);
 
         backgroundEpilIV.setImageResource(R.drawable.pro_fade);
+
+        eyesIV.setVisibility(View.INVISIBLE);
 
         conversationEpilIV.setOnClickListener(new ChangeEpilBackground());
     }
@@ -56,12 +65,22 @@ public class EpilogueActivity extends AppCompatActivity {
                     backgroundEpilIV.setImageResource(backgroundImage[i]);
                     conversationEpilTV.setText("몸이 찌뿌둥하니 악몽을 꾼 것 같네..");
                     break;
-                case 3: // case 4로 눈 깜빡이는 거 나중에 추가
-                    Intent intent = new Intent(EpilogueActivity.this, MainActivity.class);
-                    intent.addFlags(intent.FLAG_ACTIVITY_CLEAR_TOP);
-                    startActivity(intent);
-                    overridePendingTransition(R.anim.fadein, R.anim.fadeout);
-                    finish();
+                case 3:
+                    eyesIV.setVisibility(View.VISIBLE);
+                    Glide.with(EpilogueActivity.this).load(R.drawable.eyes).into(eyesIV);
+                    conversationEpilIV.setVisibility(View.INVISIBLE);
+                    conversationEpilTV.setVisibility(View.INVISIBLE);
+                    conversationIdTV.setVisibility(View.INVISIBLE);
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            Intent intent = new Intent(EpilogueActivity.this, MainActivity.class);
+                            intent.addFlags(intent.FLAG_ACTIVITY_CLEAR_TOP);
+                            startActivity(intent);
+                            overridePendingTransition(R.anim.fadein, R.anim.fadeout);
+                            finish();
+                        }
+                    }, 3000);
                     break;
             }
         }
