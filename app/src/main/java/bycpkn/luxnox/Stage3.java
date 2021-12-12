42937eefe14a94e12a4921cd99de1a9a20d79308
KyeongNa Park <61577469+pkn0814@users.noreply.github.com>
2021-12-12 20:29:36 +0900

    stage3 기울기 센서

--- app/src/main/java/bycpkn/luxnox/Stage3.java
+++ app/src/main/java/bycpkn/luxnox/Stage3.java
@@ -15,17 +15,22 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.location.GnssAntennaInfo;
+import android.media.Image;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Debug;
 import android.os.Environment;
 import android.os.Vibrator;
+import android.util.Log;
 import android.view.Display;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.MediaController;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.VideoView;
 
@@ -45,6 +50,7 @@ public class Stage3 extends AppCompatActivity {
     ImageView backgroundImg;
     ImageView setting;
     ImageView camera;
+    ImageView open;
 
     ImageView videoPaperIV;
 
@@ -59,6 +65,7 @@ public class Stage3 extends AppCompatActivity {
     private float yPos, yAccel, yVel = 0.0f;
     private float xMax, yMax;
     private Bitmap key;
+    private Bitmap opendoor;
 
     /*
         스테이지3 배경 플래그
@@ -246,15 +253,41 @@ public class Stage3 extends AppCompatActivity {
         public AccelView(Context context) {
             super(context);
             Bitmap keySrc = BitmapFactory.decodeResource(getResources(), R.drawable.st3_key);
+            Bitmap openSrc =BitmapFactory.decodeResource(getResources(), R.drawable.arrow_straight);
+
             final int dstWidth = 100;
             final int dstHeight = 200;
+
             key = Bitmap.createScaledBitmap(keySrc, dstWidth, dstHeight, true);
+            opendoor = Bitmap.createScaledBitmap(openSrc, dstWidth, dstHeight, true);
         }
         @Override
         protected void onDraw(Canvas canvas) {
             canvas.drawBitmap(key, xPos, yPos, null);
+            if(xPos >= 500 && xPos<= 600 && yPos >=1000 && yPos <=1200) { // 특정 조건에 키가 위치했을 때
+                canvas.drawBitmap(opendoor, 550, 800, null);
+                setContentView(R.layout.activity_loading);
+                Intent fin = new Intent(Stage3.this, EpilogueActivity.class);
+                startActivity(fin);
+            }
             invalidate();
         }
+/*
+        @Override
+        public boolean onTouchEvent(MotionEvent event) {
+            // 현재의 터치 액션의 종류를 받아온다.
+            int action = event.getAction();
+            // 터치 된 x좌표
+            float x = event.getX();
+            // 터치 된 y좌표
+            float y = event.getY();
+            if(x >= 500 && x <= 600 && y >= 700 && y <= 800) {
+                Intent fin = new Intent(Stage3.this, EpilogueActivity.class);
+                startActivity(fin);
+            }
+            return super.onTouchEvent(event);
+        }
+*/
     }
     @Override
     protected void onResume() {
@@ -272,7 +305,7 @@ public class Stage3 extends AppCompatActivity {
     }
 
     private void updateKey(){
-        float frameTime = 0.4f;
+        float frameTime = 0.2f;
         xVel += (xAccel * frameTime);
         yVel += (yAccel * frameTime);
 
@@ -281,16 +314,16 @@ public class Stage3 extends AppCompatActivity {
 
         xPos -= xS;								//x축 위치
         yPos -= yS;								//y축 위치
-
         if (xPos > xMax) {
             xPos = xMax;
-        } else if (xPos < 0) {
+        }
+        else if (xPos < 0) {
             xPos = 0;
         }
-
         if (yPos > yMax) {
             yPos = yMax;
-        } else if (yPos < 0) {
+        }
+        else if (yPos < 0) {
             yPos = 0;
         }
     }
